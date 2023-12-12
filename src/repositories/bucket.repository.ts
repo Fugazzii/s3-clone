c7fe435aa397caa251e78a7cdbe36e318609ea71
Fugazzii <sichinavailia@gmail.com>
2023-12-12 12:14:45 +0400

    Implemented generic repository function for all entities

--- src/repositories/bucket.repository.ts
+++ src/repositories/bucket.repository.ts
@@ -1,12 +1,12 @@
 import { pipe } from "ramda";
 import { db } from "@utils";
+import { CreateBucketDto } from "@dtos";
+import { createEntity } from "./base.repository";
 
-const selectBucketNameCount = pipe(
+export const selectBucketNameCount = pipe(
 	//! Vulnerable to sql injections
 	(name: string) => db.prepare(`SELECT COUNT(name) FROM buckets WHERE name = ${name}`),
 	Number,
 );
 
-export {
-	selectBucketNameCount
-};
+export const createBucket = createEntity<CreateBucketDto>("buckets", ["name", "owner_id"]);
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,24 +1,4 @@
-import { pipe, tryCatch } from "ramda";
-import { Result, Ok, Err } from "@sniptt/monads";
-import { db } from "@utils";
 import { CreateUserDto } from "@dtos";
+import { createEntity } from "./base.repository";
 
-const insertIntoDatabase = (createUserDto: CreateUserDto) =>
-	db.run(
-		"INSERT INTO users (email, password, username) VALUES (?, ?, ?)",
-		[createUserDto.email, createUserDto.password, createUserDto.username]
-	);
-
-const create = pipe(
-	tryCatch(
-		(dto: CreateUserDto) => Ok(insertIntoDatabase(dto)),
-		(error) => Err(error)
-	),
-	(result: Result<unknown, unknown>) => result.unwrapOrElse((err) => {
-		throw err;
-	})
-);
-
-export {
-	create
-};
+export const createUser = createEntity<CreateUserDto>("users", ["email", "password", "username"]);
